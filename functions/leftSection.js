70b5b59629164757eb6cdfe4925b146b37de3c46
Erwin Marte <erwin.mdq@gmail.com>
2023-07-18 19:25:51 -0300

    change style and displays of the body and send file to the backend

--- functions/leftSection.js
+++ functions/leftSection.js
@@ -1,7 +1,6 @@
 "use strict";
 
 // Obtein the information to show at the DOM
-// Token declarado en otra carpeta
 const currentUser = JSON.parse(localStorage.getItem('user'));
 const currentUserEmail = currentUser.email;
 
@@ -75,7 +74,7 @@ const getLogin = async () => {
             receivedInfo.loginDates.forEach((loginDate) => {
                 const formattedDate = new Date(loginDate).toLocaleString();
                 const listItem = document.createElement('li');
-                listItem.textContent = "Loggins: " + formattedDate;
+                listItem.textContent = formattedDate;
                 loginDatesList.appendChild(listItem);
             });
         } else {
@@ -86,7 +85,6 @@ const getLogin = async () => {
         console.log('Error en la solicitud:', error);
     };
 };
-
 getLogin();
 getFiles();
 
@@ -97,40 +95,58 @@ const section = document.querySelector("body > div.body-section > div.user");
 let expandedLogin = false;
 let expandedFiles = false;
 
+const upload = document.querySelector("#upload-section");
+const uploadBtn = document.querySelector("body > div.body-section > div.user > div.infor_pers > div.upload > button");
+
 const login = document.querySelector("body > div.body-section > div.user > div.infor_pers > div.logins > button > img");
-const toExpandLogin = document.querySelector("body > div.body-section > div.user > div.infor_pers > div.logins > div");
+const toExpandLogin = document.querySelector("body > div.body-section > div.info-section > div.logins-info");
 
 const files = document.querySelector("body > div.body-section > div.user > div.infor_pers > div.files > button > img");
-const toExpandFiles = document.querySelector("body > div.body-section > div.user > div.infor_pers > div.files > div");
+const toExpandFiles = document.querySelector("body > div.body-section > div.info-section > div.files-info");
 
 const expandBtn = document.querySelector("body > div.body-section > div.user > div:nth-child(2) > button");
 const unExpandBtn = document.querySelector("body > div.body-section > div.user > div:nth-child(3) > button > img");
 
+function expandUpload() {
+    upload.style.display = "block";
+    toExpandLogin.style.display ="none";
+    toExpandFiles.style.display = "none";
+};
+uploadBtn.addEventListener("click", expandUpload);
+
 // Login
 function expandLogins() {
-
     if(expandedLogin ==  false){
         toExpandLogin.style.display = "block";
+        upload.style.display ="none";
+        toExpandFiles.style.display = "none";
         expandedLogin = true;    
     }else{
         toExpandLogin.style.display = "none";
-        expandedLogin = false;    
+        upload.style.display ="none";
+        upload.style.display ="none";
+        expandedLogin = false;
+
+        expandUpload();
     };
 };
 login.addEventListener("click", expandLogins);
 
 // Files
 function expandFiles() {
-
     if(expandedFiles ==  false){
         toExpandFiles.style.display = "block";
+        upload.style.display ="none"
+        toExpandLogin.style.display = "none";
         expandedFiles = true;    
     }else{
         toExpandFiles.style.display = "none";
+        upload.style.display ="none";
         expandedFiles = false;    
+
+        expandUpload();
     };
 };
-
 files.addEventListener("click", expandFiles);
 
 // Expand bottom button
@@ -144,6 +160,5 @@ function unExpand() {
     expandBtn.style.display = "block";
     unExpandBtn.style.display = "none";
 };
-
 expandBtn.addEventListener("click", expand);
 unExpandBtn.addEventListener("click", unExpand);
