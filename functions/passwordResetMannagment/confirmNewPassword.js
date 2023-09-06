049acaea037323975f5248cff3547b62802449ba
erwin <erwin.mdq@gmail.com>
2023-09-06 01:29:19 -0300

    reset password added

--- functions/passwordResetMannagment/confirmNewPassword.js
+++ functions/passwordResetMannagment/confirmNewPassword.js
@@ -1,2 +1,86 @@
 "use strict";
 
+const form = document.getElementById("login_form");
+
+async function resetPassword(event) {
+    event.preventDefault();
+
+    const passwordInput = document.getElementById("contraseña");
+    const newPasswordInput = document.getElementById("newContraseña");
+    const password = passwordInput.value;
+    const newPassword = newPasswordInput.value;
+    const token = getQueryParam("token");
+    const email = getQueryParam("email"); // Get email from query parameters
+    const urlReset = "http://localhost:8080/api/reset/confirm";
+
+    // Make sure the passwords match
+    if (password !== newPassword) {
+        Swal.fire({
+            icon: "error",
+            title: "Error",
+            color: "rgb(51, 167, 181)",
+            text: "Las contraseñas no son iguales.",
+            confirmButtonColor: "rgb(51, 167, 181)",
+        });
+        return;
+    }
+
+    // Construct the request body
+    const requestBody = {
+        newPassword: password,
+        email: email,
+    };
+
+    // Send a POST request to your backend
+    try {
+        const response = await fetch(urlReset, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify(requestBody),
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+
+            // Handle the response from the backend here
+            if (data.message === "Password reset successful") {
+                Swal.fire({
+                    icon: "success",
+                    iconColor: "green",
+                    color: "rgb(51, 167, 181)",
+                    title: "Su contraseña fue cambiada.",
+                    text: `Cambio éxitoso ${email}`,
+                    confirmButtonColor: "rgb(51, 167, 181)",
+                });
+            } else {
+                Swal.fire({
+                    icon: "error",
+                    title: "Error",
+                    color: "rgb(51, 167, 181)",
+                    text: "Error al cambiar la contraseña.",
+                    confirmButtonColor: "rgb(51, 167, 181)",
+                });
+            }
+        } else {
+            throw new Error("Password reset request failed");
+        }
+    } catch (error) {
+        Swal.fire({
+            icon: "error",
+            title: "Error",
+            color: "rgb(51, 167, 181)",
+            text: "Error al cambiar la contraseña.",
+            confirmButtonColor: "rgb(51, 167, 181)",
+        });
+    }
+}
+
+function getQueryParam(name) {
+    const params = new URLSearchParams(window.location.search);
+    return params.get(name);
+}
+
+document.addEventListener("submit", resetPassword);
--- functions/passwordResetMannagment/emailRequest.js
+++ functions/passwordResetMannagment/emailRequest.js
@@ -1,50 +1,51 @@
 "use strict";
 
-import {emailValidator} from "../userMannagment/utils/validator.js";
-import {displayErrorAlert} from "../userMannagment/utils/displayUsers.js";
+import { emailValidator } from "../userMannagment/utils/validator.js";
+import { displayErrorAlert } from "../userMannagment/utils/displayUsers.js";
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("login_form");
     const emailInput = document.getElementById("email");
 
-    form.addEventListener("submit", function(event) {
+    form.addEventListener("submit", function (event) {
         event.preventDefault();
+
+        const resetRequest = "http://localhost:8080/api/reset/password";
 
         // Capture the value of the email
         const email = emailInput.value;
 
         // Validate the form inputs using the imported formInputValidator function
-        if(!emailValidator(email)){
+        if (!emailValidator(email)) {
             return displayErrorAlert("Email incorrecto.");
         };
 
-        //fetch("/tu/endpoint/de/backend", {
-        //    method: "POST",
-        //    headers: {
-        //        "Content-Type": "application/json"
-        //    },
-        //    body: JSON.stringify({ email: email })
-        //})
-        //.then(response => response.json())
-        //.then(data => {
-        Swal.fire({
+        fetch(resetRequest, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email: email }),
+        })
+        .then((response) => {
+            if (response.status === 200 || response.status === 201) {
+                return response.json();
+            } else {
+                throw new Error("Respuesta no válida");
+            };
+        })
+        .then((data) => {
+            Swal.fire({
                 icon: "success",
                 iconColor: "green",
                 color: "rgb(51, 167, 181)",
                 title: "Petición Enviada",
                 text: `Petición enviada con éxito a ${email}`,
-                confirmButtonColor: "rgb(51, 167, 181)"
+                confirmButtonColor: "rgb(51, 167, 181)",
             });
-        //})
-        //.catch(error => {
-        //    //console.error("Error al enviar la petición:", error);
-        //    Swal.fire({
-        //        icon: "error",
-        //        title: "Error",
-        //        color: "rgb(51, 167, 181)",
-        //        text: "Error al enviar la petición",
-        //        confirmButtonColor: "rgb(51, 167, 181)"
-        //    });
-        //});
+        })
+        .catch((error) => {
+            displayErrorAlert("Error al enviar la petición");
+        });
     });
 });
